3423d22d3cbd7091680a738e602266f56694c9c1
zarateaz <zaratepkm.3@gmail.com>
2026-07-19 08:20:13 -0500

    Actualiza APK a zarate.apk y actualiza server.py para servirlo

--- server.py
+++ server.py
@@ -782,7 +782,11 @@ class FuturisticAPIHandler(http.server.BaseHTTPRequestHandler):
             
             # Special logic for the custom APK file
             if filename == "zarate-player.apk":
-                if os.path.exists("/app/apk/zarate_music.apk"):
+                if os.path.exists("/app/apk/zarate.apk"):
+                    filepath = "/app/apk/zarate.apk"
+                elif os.path.exists("./apk/zarate.apk"):
+                    filepath = "./apk/zarate.apk"
+                elif os.path.exists("/app/apk/zarate_music.apk"):
                     filepath = "/app/apk/zarate_music.apk"
                 elif os.path.exists("./apk/zarate_music.apk"):
                     filepath = "./apk/zarate_music.apk"
